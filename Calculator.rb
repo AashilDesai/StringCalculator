97df0e8b0aa53d766dcc9ebdba22f768a8e7da2d
Aashil Desai <aashildesai@SKs-MacBook-Pro.local>
2016-07-20 11:07:07 -0400

    Turns out in Ruby the method is called STRING.size, not STRING.length like in Java

--- Calculator.rb
+++ Calculator.rb
@@ -2,11 +2,11 @@
 #The goal of StringCalculator is help me learn RSpec
 
 def sumString(str)
-	if str.length = 0 #empty string
+	if str.size == 0 #empty string
 		return 0
 	else
 		split = str.partition(',')
-		if split[1] = ',' #meaning that there was a successful partition
+		if split[1] == ',' #meaning that there was a successful partition
 			return split[0].to_i + split[2].to_i
 		else #there was no ',' in the str
 			return str.to_i
