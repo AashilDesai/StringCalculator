b60099c3a194af3e6e9235ce6e45eeea8636c381
Aashil Desai <aashildesai@SKs-MacBook-Pro.local>
2016-07-20 11:28:06 -0400

    More refactoring, plus tweaked UI

--- Calculator.rb
+++ Calculator.rb
@@ -3,14 +3,17 @@
 
 def sumString(str)
 	split = str.partition(',')
-	if split[1] == ',' #meaning that there was a successful partition
-		#recursively handles issue to repartition
+
+	if split[1] == ',' #there was a ',', meaning there's more than one argument
+		#recursively handles issue
 		return split[0].to_i + sumString(split[2])
-	else #there was no ',' in the str
+	else 
 		return str.to_i
 	end
 end
 
+puts "-" * 10
 puts "Enter the string you wish to sum (separate the 2 numbers by a comma)"
 sum = sumString(gets.chomp)
 puts "The sum is: #{sum}"
+puts "-" * 10
