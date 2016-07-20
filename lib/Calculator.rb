cad83cdaf4aae1f27f6bf96c505766eff3095289
Aashil Desai <aashildesai@SKs-MacBook-Pro.local>
2016-07-20 13:45:15 -0400

    Turned Calculator into a module so that I could use it for testing purposes

--- lib/Calculator.rb
+++ lib/Calculator.rb
@@ -1,19 +1,22 @@
 #This is the actual calculator of String-Calculator
 #The goal of StringCalculator is help me learn RSpec
+module Calculator
+	def sumString(str)
+		split = str.partition(',')
 
-def sumString(str)
-	split = str.partition(',')
-
-	if split[1] == ',' #there was a ',', meaning there's more than one argument
-		#recursively handles issue
-		return split[0].to_i + sumString(split[2])
-	else 
-		return str.to_i
+		if split[1] == ',' #there was a ',', meaning there's more than one argument
+			#recursively handles issue
+			return split[0].to_i + sumString(split[2])
+		else 
+			return str.to_i
+		end
 	end
 end
 
+include Calculator
+
 puts "-" * 10
 puts "Enter the string you wish to sum (separate the numbers with commas)"
-sum = sumString(gets.chomp)
+sum = Calculator.sumString(gets.chomp)
 puts "The sum is: #{sum}"
 puts "-" * 10
